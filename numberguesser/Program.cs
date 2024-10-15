b4e4d7e04ecc694487492353cc3d6046c113c0e6
JEEVEEKA <jeeveeka.ps@kalvium.community>
2024-10-15 18:23:45 +0530

    completed numberguesser game using c#

---
+++ numberguesser/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace NumberGuesser
+{
+    class Program
+    {
+        // Entry point method
+        static void Main(string[] args)
+        {
+            GetAppInfo();
+
+            GreetUser();
+
+            while (true)
+            {
+                // Create random object
+                Random random = new Random();
+                int correctNumber = random.Next(1, 10);
+
+                // Guess the number
+                int guess = 0;
+                Console.WriteLine("Guess a number between 1 and 10");
+
+                while (guess != correctNumber)
+                {
+                    string input = Console.ReadLine();
+
+                    // Ensure it is a number
+                    if (!int.TryParse(input, out guess))
+                    {
+                        PrintColorMessage(ConsoleColor.Red, "Please enter an actual number");
+                        continue;
+                    }
+
+                    // Compare the guess to the correct number
+                    if (guess != correctNumber)
+                    {
+                        PrintColorMessage(ConsoleColor.Red, "Wrong number, please try again");
+                    }
+                }
+
+                // Output success message
+                PrintColorMessage(ConsoleColor.Green, "Congrats, you are correct !");
+
+                // Ask to play again
+                Console.WriteLine("Play Again? [Y or N]");
+                string answer = Console.ReadLine().ToUpper();
+
+                if (answer == "Y")
+                {
+                    continue;
+                }
+                else if (answer == "N")
+                {
+                    return;
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+        static void GetAppInfo(){
+            // Set app vars
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Jeeveeka";
+
+            // Change text color
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            // Write out app info
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+            // Reset text color
+            Console.ResetColor();
+        }
+
+        static void GreetUser(){
+            // Ask user's name
+            Console.WriteLine("What is your name?");
+
+            // Get user input
+            string inputName = Console.ReadLine();
+            Console.WriteLine("Hello {0}, Let's play a game...", inputName);
+        }
+
+        static void PrintColorMessage(ConsoleColor color, string message){
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
